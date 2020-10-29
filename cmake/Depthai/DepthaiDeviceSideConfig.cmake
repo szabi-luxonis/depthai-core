57da9342a395b78dc4c531fabda7f839df88074a
saching13 <sachin.guruswamy@gmail.com>
2020-10-29 13:12:19 -0700

    merged with main and added testing features

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "91f4d000d1509845b4e5da19fbf99c7b6f5af1a1")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e17180b043c35d451ec4147e85c10621a3d796a9")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
