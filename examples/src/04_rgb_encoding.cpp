2f0ab62cb3f4946b98065f6560b14df6c1813471
Łukasz Piłatowski <lukpila29@gmail.com>
2021-03-01 22:02:19 +0100

    migrate examples 16 and 21

--- examples/src/04_rgb_encoding.cpp
+++ examples/src/04_rgb_encoding.cpp
@@ -13,11 +13,11 @@ int main(int argc, char** argv){
     using namespace std;
     using namespace std::chrono;
 
-    std::string h264Path("video.h264");
+    std::string h265Path("video.h265");
     
     // If path specified, use that
     if(argc > 1){
-        h264Path = std::string(argv[1]);
+        h265Path = std::string(argv[1]);
     }
 
     dai::Pipeline p;
@@ -28,17 +28,17 @@ int main(int argc, char** argv){
     auto videnc = p.create<dai::node::VideoEncoder>();
 
     // XLinkOut
-    xout->setStreamName("h264");
+    xout->setStreamName("h265");
     xout2->setStreamName("preview");
 
     // ColorCamera    
     colorCam->setPreviewSize(300, 300);
-    colorCam->setResolution(dai::ColorCameraProperties::SensorResolution::THE_1080_P);
+    colorCam->setResolution(dai::ColorCameraProperties::SensorResolution::THE_4_K);
     //colorCam->setFps(5.0);
     colorCam->setInterleaved(true);
 
     // VideoEncoder
-    videnc->setDefaultProfilePreset(1920, 1080, 30, dai::VideoEncoderProperties::Profile::H264_MAIN);
+    videnc->setDefaultProfilePreset(3840, 2160, 30, dai::VideoEncoderProperties::Profile::H265_MAIN);
 
     // Link plugins CAM -> XLINK
     colorCam->video.link(videnc->input);
@@ -50,17 +50,17 @@ int main(int argc, char** argv){
     // Start the pipeline
     d.startPipeline();
 
-    auto myfile = std::fstream(h264Path, std::ios::out | std::ios::binary);
+    auto myfile = std::fstream(h265Path, std::ios::out | std::ios::binary);
 
 
-    auto h264Queue = d.getOutputQueue("h264", 8, false);
+    auto h265Queue = d.getOutputQueue("h265", 8, false);
     auto previewQueue = d.getOutputQueue("preview", 8, false);
     while(1){
 
         auto preview = previewQueue->get<dai::ImgFrame>();
         cv::imshow("preview", cv::Mat(preview->getHeight(), preview->getWidth(), CV_8UC3, preview->getData().data()));
-        auto h264 = h264Queue->get<dai::ImgFrame>();
-        myfile.write((char*)h264->getData().data(), h264->getData().size());
+        auto h265 = h265Queue->get<dai::ImgFrame>();
+        myfile.write((char*)h265->getData().data(), h265->getData().size());
 
         int key = cv::waitKey(1);
         if (key == 'q'){
@@ -69,8 +69,8 @@ int main(int argc, char** argv){
     }
     myfile.close();
 
-    std::cout << "To view the encoded data, convert the stream file " << h264Path << " into a video file (.mp4) using a command below:" << std::endl;
-    std::cout << "ffmpeg -framerate " << colorCam->getFps() << " -i " << h264Path << " -c copy video.mp4" << std::endl;
+    std::cout << "To view the encoded data, convert the stream file " << h265Path << " into a video file (.mp4) using a command below:" << std::endl;
+    std::cout << "ffmpeg -framerate " << colorCam->getFps() << " -i " << h265Path << " -c copy video.mp4" << std::endl;
 
     return 0;
 }
