660b90c58e46f1daad6b40ef4931ea6c6a76a610
alex-luxonis <alex@luxonis.com>
2021-03-13 04:26:42 +0200

    Update FW: cherry-pick 'gen2_nn_shape_fix' changes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0f1ac77644d6fd0ce75485e9fa20d7e34ae23ce7")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8c696cbb0a317968786113f6c9f98a59b20dd8e0")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
